dfde44be2389e106216b90941c0a0b45d5f0a8d0
DaretSaret <Chaikodenis6554@gmail.com>
2025-02-26 04:26:18 +0700

    Task6

---
+++ Task6/src/ThreadSafety(2)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ThreadSafety_2_
+{
+    public class Program
+    {
+        static int counter = 0;
+
+        private static object protector = new object();
+
+        static void Main()
+        {
+
+            Thread thread = new Thread(new ThreadStart(Together));
+            Thread thread1 = new Thread(new ThreadStart(Together));
+            thread.Start();
+            thread1.Start();
+            thread.Join();
+            thread1.Join();
+
+            Console.WriteLine(counter);
+        }
+        static void Together()
+        {
+            lock (protector)
+            {
+                for (int i = 0; i < 100000; i++) //изменил на 100к ибо у меня не было ошибок в подсчете
+                {
+                    counter++;
+                }
+            }
+        }
+    }
+}
---
+++ Task6/src/asyncAwait(5)/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace asyncAwait_5_
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var url = new string[]
+            {
+            "https://google.com",
+            "https://example.com",
+            "https://microsoft.com"
+            };
+
+            using (HttpClient client = new HttpClient())
+            {
+                var task = new Task<string>[url.Length];
+
+                for (int i = 0; i < url.Length; i++)
+                {
+                    task[i] = client.GetStringAsync(url[i]);
+                }
+
+                try
+                {
+                    var results = await Task.WhenAll(task);
+
+                    for (int i = 0; i < results.Length; i++)
+                    {
+                        Console.WriteLine("Содержимое страницы: " + (url[i]) + " " + (results[i].Length) + " символов");
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("Ошибка при загрузке данных " + (e.Message));
+                }
+            }
+        }
+    }
+}
